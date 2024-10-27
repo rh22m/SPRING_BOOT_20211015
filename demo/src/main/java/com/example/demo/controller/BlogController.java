56eb70b7226a84ca2cc4affed3b5ae7c9a513d2a
rh22m <simonrheem@sungkyul.ac.kr>
2024-10-27 18:12:36 +0900

    5주차 (Resy API)

---
+++ demo/src/main/java/com/example/demo/controller/BlogController.java
@@ -0,0 +1,47 @@
+package com.example.demo.controller;
+import com.example.demo.model.service.BlogService;
+import java.util.List;
+//import org.hibernate.mapping.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import com.example.demo.model.domain.Article;
+
+
+@Controller // 컨트롤러 어노테이션 명시
+public class BlogController {
+
+//@GetMapping("/hello") // 전송 방식 GET
+//public String hello(Model model) {
+//model.addAttribute("data", " 방갑습니다."); // model 설정
+//return "hello"; // hello.html 연결
+//}
+
+//@GetMapping("/about_detailed")
+//public String about() {
+//return "about_detailed";
+//}
+
+//@GetMapping("/test1")
+//public String thymeleaf_test1(Model model) {
+//model.addAttribute("data1", "<h2> 방갑습니다 </h2>");
+//model.addAttribute("data2", "태그의 속성 값");
+//model.addAttribute("link", 01);
+//model.addAttribute("name", "홍길동");
+//model.addAttribute("para1", "001");
+//model.addAttribute("para2", 002);
+//return "thymeleaf_test1";
+//}
+
+@GetMapping("/article_list") //게시판 링크 지정
+public String article_list(Model model) {
+    List<Article> list = blogService.findAll(); //게시판 리스트
+    model.addAttribute("articles", list); //모델에 추가
+    return "article_list"; //.html 연결
+}
+
+@Autowired
+BlogService blogService; // DemoController 클래스 아래 객체 생성
+
+}
